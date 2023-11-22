bc3e67056f34164486be1f1e6a156c9b49173506
Emir <emirseyhana.z@gmail.com>
2023-11-22 16:56:21 +0300

    Initial Commit

---
+++ VerilerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VerilerSwiftUI
+//
+//  Created by Emir Seyhan on 22.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerilerSwiftUI/VerilerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerilerSwiftUIApp.swift
+//  VerilerSwiftUI
+//
+//  Created by Emir Seyhan on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct VerilerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
